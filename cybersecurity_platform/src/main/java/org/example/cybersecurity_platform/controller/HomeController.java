d3b6a9bf709a1aac2301265f16d07698a9616324
George <jorjacus@Georges-MacBook-Pro.local>
2025-05-04 20:27:06 +0300

    Created news and About page

---
+++ cybersecurity_platform/src/main/java/org/example/cybersecurity_platform/controller/HomeController.java
@@ -0,0 +1,18 @@
+package org.example.cybersecurity_platform.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+
+}
