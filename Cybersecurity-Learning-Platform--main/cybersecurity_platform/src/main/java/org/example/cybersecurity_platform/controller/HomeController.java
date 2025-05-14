cb368d1c1eba927d7261cfe973bbff9d714d387e
Ionescu Rares-Andrei <82729871+RaresI@users.noreply.github.com>
2025-05-14 20:28:36 +0300

    DB
    Created the DB, User Profile Page and Login/Register with SpringSecurity

---
+++ Cybersecurity-Learning-Platform--main/cybersecurity_platform/src/main/java/org/example/cybersecurity_platform/controller/HomeController.java
@@ -0,0 +1,34 @@
+package org.example.cybersecurity_platform.controller;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+
+    @GetMapping("/news")
+    public String news() {
+        return "home";
+    }
+
+    @GetMapping("/login")
+    public String loginForm() {
+        return "login";
+    }
+
+}
